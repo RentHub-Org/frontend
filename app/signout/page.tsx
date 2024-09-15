f8e631748fb3bbb9ec2ae2c058b8116d3ba66d3c
priyanshu_owner <67639480+priyanshu-7938@users.noreply.github.com>
2024-09-15 15:58:40 +0530

    ground zero

---
+++ app/signout/page.tsx
@@ -0,0 +1,37 @@
+'use client'    
+import { signOut, useSession } from "next-auth/react";
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from "react";
+import React from "react";
+import { BackgroundBeamsWithCollision } from "@/components/ui/background-beams-with-collision";
+import { IconLinkPlus } from "@tabler/icons-react";
+import { ExternalLink } from "lucide-react";
+
+export default function SignOut() {
+    const router = useRouter();
+    const session = useSession();
+    useEffect(()=>{
+        if(session.status === "authenticated"){
+            signOut();
+        }
+    },[session])
+    return (
+    <BackgroundBeamsWithCollision>
+        <h2 className="flex gap-2 flex-col items-center text-2xl relative z-20 md:text-4xl lg:text-7xl font-bold text-center text-white font-sans tracking-tight">
+            <div>
+
+            <span className="bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r from-theme-1 to-[#b4b2b2]">How was it? Experiencing{" "}</span>
+            <div className="relative mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
+            <div className="absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r py-4 from-theme-3 via-theme-4 to-theme-3 [text-shadow:0_0_rgba(0,0,0,0.1)]">
+                <span className="">Flux.</span>
+            </div>
+            <div className="relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-theme-3 via-theme-4 to-theme-3 py-4">
+                <span className="">Flux.</span>
+            </div>
+            </div>
+            </div>
+            <span className="text-sm tracking-tight font-sans flex">You have logged out.<button className="flex gap-1 items-center text-theme-3" onClick={()=>{router.push("signin")}}>Signin here <ExternalLink size={15}/></button></span>
+        </h2>
+    </BackgroundBeamsWithCollision>
+  );
+}
